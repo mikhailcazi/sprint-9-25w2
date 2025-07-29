0feb13c0488791b7daac7d10153fedd56f709d5b
Mikhail Cazi <mikhailcazi@gmail.com>
2025-07-30 02:38:10 +0530

    Class 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import "./css/styles.css";
 import Counter from "./week-1/Counter";
+import Form from "./week-2/form";
 import UpgradedCounter from "./week-2/UpgradedCounter";
 
 function App() {
   return (
     <>
-      <UpgradedCounter />
+      <Form />
     </>
   );
 }
---
+++ src/week-2/form.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
+
+function Form() {
+  const [fName, setFName] = useLocalStorage("fName", "");
+  const [lName, setLName] = useLocalStorage("lName", "");
+  const [theme, setTheme] = useState("light");
+
+  const handleSubmit = () => {
+    console.log("Submitted");
+  };
+
+  const themeButtonClicked = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <>
+      <div className={theme}>
+        First Name:{" "}
+        <input
+          type="text"
+          name="fName"
+          value={fName}
+          onChange={(e) => setFName(e.target.value)}
+        />
+        <br />
+        Last Name:{" "}
+        <input
+          type="text"
+          name="lName"
+          value={lName}
+          onChange={(e) => setLName(e.target.value)}
+        />
+        <br />
+        <button type="button" onClick={themeButtonClicked}>
+          Switch to {theme === "light" ? "dark" : "light"} theme!
+        </button>
+        <br />
+        <button type="button" onClick={handleSubmit}>
+          Submit
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default Form;
